c52014e23cd1706c11b4cec66ddbc287656ba0ac
kuquert <marcus.kuquert@icloud.com>
2025-02-16 19:57:06 -0300

    Extract non-generic strings to Config struct

---
+++ Boolder/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  Boolder
+//
+//  Created by Marcus Kuquert on 2025-02-16.
+//
+
+import Foundation
+
+struct Config {
+    static let styleUrl = "mapbox://styles/nmondollot/cl95n147u003k15qry7pvfmq2"
+    static let problemsTilesetIdUrl = "mapbox://nmondollot.4xsv235p"
+    static let circuitsTilesetIdUrl = "mapbox://nmondollot.11sumdgh"
+}
